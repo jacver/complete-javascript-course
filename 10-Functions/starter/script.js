1ff45efbc6dfee7ff806068df6eb732849725578
Jacob Vernau <jacobvernau@gmail.com>
2021-11-17 18:25:04 -0700

    Update script.js
    lessons 131-134

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -99,3 +99,109 @@ document.body.addEventListener('click', high5);
 
 ['jonas', 'martha', 'adam'].forEach(high5);
 // passing a callback FOR EACH of the array elements
+
+// Lesson 132 - functions returning new functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+// greeterHey('Jacob');
+// greet('Hello')('Jacob');
+
+// challenge - rewrite in arrow fn
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+// greetArrow('Hello')('Jacob');
+
+// Lesson 133 - The CALL and APPLY methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+    // adds a new object with booking info to empty bookings array
+  },
+};
+
+const book = lufthansa.book;
+// this creates and external function to minimize repeat code for other airlines
+
+// lufthansa.book(1034, 'Jacob Vernau');
+// lufthansa.book(3923, 'April Beckwith');
+// console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+  book,
+};
+
+// book(123, 'Jacob Vernau') will not work, needs to be assigned
+book.call(lufthansa, 239, 'April Beckwith');
+book.call(eurowings, 23, 'Jacob Vernau');
+// first argument is the this. keyword
+
+// apply method
+const flightData = [583, 'April Beckwith'];
+book.apply(lufthansa, flightData);
+// apply method is not as useful in modern JS in favor of spread operator to remove from array and insert into function
+
+// Video 134 - the bind method
+
+const bookEW = book.bind(eurowings);
+// this does not call the book function. It returns new function where this keyword is always set to eurowings
+const bookLH = book.bind(lufthansa);
+// we can create one for each airline to simplify process for booking information
+bookEW(1234, 'Jacob Vernau');
+
+const bookEW23 = book.bind(eurowings, 23);
+// setting number in stone so now we just need to call with name
+// this is called partial application - parts of original function are already applied
+bookEW23('Jacob Vernau');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// you must manually define the this keyword by binding lufthansa. Without this, it points to the higher order function (Buy)
+// must master bind method
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+// when using partial application the thing you want to preset has to be first argument!!
+console.log(addTax(0.1, 200));
+// general function to add tax
+
+const addVAT = addTax.bind(null, 0.23);
+// there is no this keyword so you must set it as a null value. This allows it to read the rate you want because the first argument is always THIS keyword
+console.log(addVAT(1000));
+
+// summary - BIND creates a new function. CALL calls the same function.
+
+// challenge - recreate the tax example using a function that returns another function
+
+const addTax2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+// first function takes in rate, the second one needs function.
+const addVAT2 = addTax2(0.23);
